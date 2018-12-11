310df7b9502873e7633c4db9aabbca5c10882058
WillyCrespo <36381521+WillyCrespoGD@users.noreply.github.com>
2018-12-10 21:22:57 -0500

    Create porn.bat

---
+++ porn.bat
@@ -0,0 +1,12 @@
+@ECHO ON
+pause
+echo Never abuse your porn :)
+echo.
+pause
+echo Creating porn,press any key to confirm action
+echo.
+pause
+echo @Folder Making = 100@  @ Exiting Program ,if you press any key :)@
+md porn
+pause
+exit
